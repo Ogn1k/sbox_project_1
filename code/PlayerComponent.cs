fdd672ec36fcc793d3e265d4dec2741fd4d112a0
Kpyxa <48378742+Kpyxa@users.noreply.github.com>
2025-06-27 12:55:18 +0500

    Animation Setup step 1

--- code/PlayerComponent.cs
+++ code/PlayerComponent.cs
@@ -24,6 +24,10 @@ public sealed class PlayerComponent : Component
 	public SkinnedModelRenderer weaponModelrend { get; set; }
 
 	[Property]
+	[Category( "Components" )]
+	public CitizenAnimationHelper wepAnimHelper { get; set; }
+
+	[Property]
 	[Category( "Stats" )]
 	[Range( 50f, 200f, 10f )]
 	public float PunchRange { get; set; } = 100f;
@@ -39,15 +43,90 @@ public sealed class PlayerComponent : Component
 	public float PunchCooldown { get; set; } = 0.5f;
 
 	public TimeUntil NextPunch;
+	public bool WepDeployed { get; set; } = false;
+	public int CurCombo { get; set; } = 0;
+	public bool CanCombo { get; set; } = false;
 	private ModelPhysics _ragdoll;
 	private Vector3 _spawnPosition;
 	private TimeUntil _resetPose;
-	
+
+	protected override void OnStart()
+	{
+		ModelRenderer.OnGenericEvent += HandleGenericEvent;
+		ModelRenderer.OnAnimTagEvent += HandleAnimTagEvent;
+	}
+
+	private void HandleGenericEvent(SceneModel.GenericEvent eventData)
+	{
+		if (eventData.Type == "change_deploy")
+		{
+			WepDeployed = !WepDeployed;
+
+			SharedParams("sword_deployed", WepDeployed);
+		}
+		else if (eventData.Type == "change_combo")
+		{
+			if (CurCombo == 3)
+				{
+				CurCombo = 0;
+				}
+			else
+				{
+				CurCombo += 1;
+				}
+		}
+	}
+
+	private void HandleAnimTagEvent( SceneModel.AnimTagEvent tagEvent )
+	{
+		if ( tagEvent.Name == "continue_combo" )
+		{
+			if ( tagEvent.Status == SceneModel.AnimTagStatus.Start )
+			{
+				CanCombo = true;
+			}
+			else if ( tagEvent.Status == SceneModel.AnimTagStatus.End )
+			{
+				CanCombo = false;
+			}
+		}
+	}
+
+	public void SharedParams(string parameter, object value)
+	{
+		switch (value)  
+		{  
+			case int number:  
+				ModelRenderer.Set( parameter, number );
+				weaponModelrend.Set( parameter, number );
+				break;  
+
+			//case string text: 
+				//ModelRenderer.Set( parameter, text );
+				//weaponModelrend.Set( parameter, text );  
+				//break;  
+
+			case bool flag:
+				ModelRenderer.Set( parameter, flag );
+				weaponModelrend.Set( parameter, flag );   
+				break;
+
+			case float fnumber:
+				ModelRenderer.Set( parameter, fnumber );
+				weaponModelrend.Set( parameter, fnumber );   
+				break;
+
+			case null:  
+				break;  
+
+			default:  
+				break;  
+		} 
+	}
+
     public void Punch()
 	{
-		ModelRenderer.Set( "holdtype", 6 );
-		ModelRenderer.Set( "b_attack", true );
-		weaponModelrend.Set( "b_attack", true );
+		SharedParams("b_attack", true);
 		_resetPose = 3f;
 
 		var punchDirection = Controller.EyeAngles.Forward;
@@ -66,7 +145,12 @@ public sealed class PlayerComponent : Component
 
 		unit.Damage( PunchDamage );
 	}
-    
+
+    public void DeploySword()
+	{
+		SharedParams("b_deploy", true);
+	}
+
     [Button]
 	public void Ragdoll()
 	{
@@ -106,19 +190,43 @@ public sealed class PlayerComponent : Component
 		animationHelper.WithWishVelocity( WorldPosition.Normal );
 		//animationHelper.AimAngle = Head.Transform.Rotation;
     	animationHelper.IsGrounded = Controller.IsOnGround;
+
+		if ( wepAnimHelper == null )
+			return;
+		
+		wepAnimHelper.WithVelocity( Controller.Velocity );
+		wepAnimHelper.WithWishVelocity( WorldPosition.Normal );
+		//animationHelper.AimAngle = Head.Transform.Rotation;
+    	wepAnimHelper.IsGrounded = Controller.IsOnGround;
     
 	}
 
 	protected override void OnFixedUpdate()
 	{
-		if ( Input.Down( "attack1" ) && NextPunch )
+		if ( Input.Down( "attack1" ) )
+			{
+			if (!NextPunch && CanCombo)
+				{
+					SharedParams("combo", CurCombo);
+					Punch();
+					NextPunch = PunchCooldown;
+				}
+			else if (NextPunch)
+				{
+					CurCombo = 0;
+					SharedParams("combo", CurCombo);
+					Punch();
+					NextPunch = PunchCooldown;
+				}
+			}
+
+		if ( Input.Down( "reload" ) && NextPunch )
 		{
-			Punch();
+			DeploySword();
 			NextPunch = PunchCooldown;
 		}
-
-		if ( _resetPose )
-			ModelRenderer.Set( "holdtype", 0 );
+		//if ( _resetPose )
+			//ModelRenderer.Set( "holdtype", 0 );
 		UpdateAnimation();
 	}
 }
